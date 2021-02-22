68a9b114faf67baae3939b3cbbc9d2a6b81859ee
Jot27th <62381923+Radmir27th@users.noreply.github.com>
2021-02-22 21:54:12 +0300

    Add the main file

---
+++ svr/FinderPath.cpp
@@ -0,0 +1,42 @@
+﻿
+
+
+#include "PathAlgorithm.h"
+
+int main()
+{
+	RenderWindow *window = new RenderWindow(VideoMode(1500, 900), "PathFinder");
+	
+	PathAlgorithm grid(window);
+	
+	while (window->isOpen()) // Main Loop
+	{
+
+		Event event;
+		while (window->pollEvent(event)) {
+			if (event.type == Event::Closed)
+				window->close();
+		}
+
+		grid.run();
+		grid.m_time++;
+		if (grid.m_mode == GridManip::SEARCH)
+		{
+			grid.DijkstrasAlgo();
+			grid.m_mode = GridManip::END;
+
+		}
+		window->clear(Color::White);
+
+		grid.drawTheSearch();
+		grid.drawThePath();
+		grid.drawGrid();
+		grid.drawNode();
+
+
+		window->display();
+	}
+	return 0;
+    
+}
+
